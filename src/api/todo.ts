0cc933a7a8cfd2439c287c480fcacd08b4f581bd
amrutapasarkar08 <amrutapasarkar@gamil.com>
2025-07-24 08:32:16 +1200

    added env file

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,7 +1,8 @@
 import type { Todo } from '../types/todo';
+const apiUrl = import.meta.env.VITE_Todo_API_URL;
 
 export const fetchTodos = async (): Promise<Todo[]> => {
-  const response = await fetch('https://dummyjson.com/todos');
+  const response = await fetch(apiUrl);
   if (!response.ok) throw new Error('Network error');
   const data = await response.json();
   return data.todos;
